5fbef368d329ccb75190298f986d341b1e7f434f
Corwin <info@corwindev.nl>
2023-06-15 18:13:42 +0200

    fixed hover bug

--- resources/views/resources/show.blade.php
+++ resources/views/resources/show.blade.php
@@ -81,7 +81,7 @@
                                 </a>
                             @else
                                 <button id="dropdownHoverButton" data-dropdown-toggle="dropdownHover"
-                                    data-dropdown-trigger="hover"
+                                    
                                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                                     type="button">Buy <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none"
                                         stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -111,7 +111,7 @@
                             @endif
                         @else
                             <button id="dropdownHoverButton" data-dropdown-toggle="dropdownHover"
-                                data-dropdown-trigger="hover"
+                                
                                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                                 type="button">Buy <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none"
                                     stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -147,7 +147,7 @@
                             </a>
                         @else
                             <button id="dropdownHoverButton" data-dropdown-toggle="dropdownHover"
-                                data-dropdown-trigger="hover"
+                                
                                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                                 type="button">Buy <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none"
                                     stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
